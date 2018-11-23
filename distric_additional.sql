de5fa7a0803a0886395e772f7a40498e9c6fbba0
niangzizenyang <zero771364328@gmail.com>
2018-11-23 17:59:17 +0800

    m1

---
+++ distric_additional.sql
@@ -0,0 +1,13 @@
+create table adv_info.district_additional(
+guid integer
+,nbhdname varchar(100)
+,developers varchar(100)
+,buideyear integer
+,floornum integer
+,roomnum varchar(20)
+,buildarea decimal(20,4)
+,propertycom varchar(100)
+,elevatornum integer
+,gd_id varchar(20)
+,primary key (guid) 
+); 
